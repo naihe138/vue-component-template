485dcb37390905b4700480e8db81c178a37043b1
naihe138 <370215230@qq.com>
2022-05-29 10:54:55 +0800

    feat: build

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const { readFileSync, rmSync, writeFileSync } = require('fs')
 const { exec, cp } = require('shelljs')
 const path = require('path')
-
+const { name = 'my-component' } = require('./package.json')
 function readWriteFileSync(path, callback) {
   writeFileSync(path, callback(readFileSync(path, { encoding: 'utf8' })), { encoding: 'utf8' })
 }
@@ -15,13 +15,14 @@ function readRemoveFileSync(path, callback) {
 const resolvePath = url => path.resolve(__dirname, url);
 const replace = (source, target, text) => source.replace(target, text)
 async function build_library() {
-  exec('rimraf ./dist')
-  // exec('vue-tsc --noEmit && vite build');
-  // readRemoveFileSync(resolvePath('./dist/style.css'), css => {
-  //   readWriteFileSync(resolvePath('./dist/my-component.es.js'), js => replace(js, `"__STYLE_CSS__TEXT"`, "`"+ css.trim() +"`"))
-  //   readWriteFileSync(resolvePath('./dist/my-component.umd.js'), js => replace(js, `"__STYLE_CSS__TEXT"`, "`"+ css.trim() +"`"))
-  // });
-  // cp(resolvePath('./dist/src/components/my-component.vue.d.ts'), resolvePath('./dist/my-component.d.ts'));
+  exec('rimraf ./dist');
+  exec('vue-tsc --noEmit && vite build');
+  readRemoveFileSync(resolvePath('./dist/style.css'), css => {
+    readWriteFileSync(resolvePath(`./dist/${name}.es.js`), js => replace(js, `"__STYLE_CSS__TEXT"`, "`"+ css.trim() +"`"))
+    readWriteFileSync(resolvePath(`./dist/${name}.umd.js`), js => replace(js, `"__STYLE_CSS__TEXT"`, "`"+ css.trim() +"`"))
+  });
+  cp(resolvePath(`./dist/src/components/${name}.vue.d.ts`), resolvePath(`./dist/${name}.d.ts`));
+  exec('rimraf ./dist/src');
 }
 
-build_library()
+build_library();
